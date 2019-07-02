a2038ea0d70a1fd819081671b65c07537446897c
Cheng Tan <chetan@microsoft.com>
2019-07-02 13:21:14 -0400

    problem framing mobile working

--- assets/nav.js
+++ assets/nav.js
@@ -9,6 +9,13 @@ function myFunction() {
 
 $(document).ready(function() {
   const $nav = $(".nav_bar_container");
+  const modules = [
+    'reinforcement',
+    'versatility',
+    'online',
+    'speed',
+    'input_format'
+  ]
 
   $(document).scroll(function () {
     const scroll_top = $(this).scrollTop();
@@ -28,7 +35,7 @@ $(document).ready(function() {
 
   });
 
-  $(".tabs_container").on("click", ".nav button", function() {
+  $(".tabs_container").on("click", ".nav .nav_item", function() {
     const $this = $(this);
     showModule($this);
   });
@@ -41,6 +48,27 @@ $(document).ready(function() {
   $(".tabs_container").on("click", ".sub_nav button", function() {
     const $this = $(this);
     showModule($this);
+  });
+
+  $(".tabs_container").on("click", "button.arrow", function() {
+    const $this = $(this);
+    const class_names = $this.attr("class");
+    let module_id = $this.attr('data-current-module');
+
+    let index = modules.findIndex((module) => {
+      return module === module_id
+    });
+
+    if (class_names.includes('previous')) {
+      index = ((index - 1) + modules.length) % modules.length;
+    } else {
+      index = (index + 1) % modules.length;
+    }
+
+    $(".tabs_container .arrow").attr("data-current-module", modules[index]);
+    const $module = $("div[data-module_id=" + modules[index] +"]");
+    $module.siblings().hide();
+    $module.show();
   });
 });
 
