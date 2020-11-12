a6ed3f19165441d08fbdaf710457ebfb2d784fa0
Cheng Tan <chetan@microsoft.com>
2020-11-12 12:22:59 -0500

    Remove unused js

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -4,14 +4,7 @@ const blogModule = (function() {
 
   function init() {
     cacheDom();
-    addTutorialNav();
-    addLinkToHeader();
-    highlightSectionLink();
     bindButtonEvent();
-
-    $(document).scroll(function () {
-      highlightSectionLink();
-    });
   }
 
   return {
@@ -19,54 +12,7 @@ const blogModule = (function() {
   }
 
   function cacheDom() {
-    DOM.$tutorial_nav = $(".tutorial_container .tutorial_nav .tutorial_content_nav");
-    DOM.$h2s = $(".tutorial_container h2");
     DOM.$button = $(".blog_container button");
-  }
-
-  function addTutorialNav() {
-    const tutorial_nav = Array.from(DOM.$h2s.map(function(_, h2) {
-      const $h2 = $(h2);
-      const id = $(h2).attr('id');
-
-      return (
-        "<li>" +
-          "<span>" +
-            "<a href=#" + id + ">" +
-              $h2.contents().get(0).nodeValue +
-            "</a>" +
-          "</span>" +
-        "</li>");
-    })).join('');
-
-    DOM.$tutorial_nav.append(tutorial_nav);
-  }
-
-  function addLinkToHeader() {
-    DOM.$h2s.each(function(_, h2) {
-      const $h2 = $(h2);
-      const id = $h2.attr('id');
-      const icon = '<i class="fa fa-link"></i>';
-
-      if (id) {
-        return $h2.append($("<a />").attr("href", "#" + id).html(icon));
-      }
-    });
-  }
-
-  function highlightSectionLink() {
-    const current_position = $(window).scrollTop();
-
-    DOM.$tutorial_nav.find('a').each(function() {
-      const $section_link = $(this);
-      const $section = $($section_link.attr('href'));
-
-      if ($section.position().top <= current_position &&
-        $section_link.offset().top + $section.height() > current_position) {
-        DOM.$tutorial_nav.children('li').removeClass('active');
-        $section_link.closest('li').addClass('active');
-      }
-    });
   }
 
   function bindButtonEvent() {
